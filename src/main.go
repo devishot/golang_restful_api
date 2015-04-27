2e129305670183da44faa50e565a0ddbf9bc166d
Sattar Stamkulov <sattar.coder@gmail.com>
2015-04-27 09:18:21 +0600

    added

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 // "server_api"
 import (
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
 
@@ -10,6 +12,11 @@ import (
 
 
 func main() {
+  db, err := sql.Open("sqlite3", "dbfile.db")
+  checkErr(err)
+  defer db.Close()
+
+
   m := martini.Classic()
 
   // Setup middleware
@@ -24,15 +31,37 @@ func main() {
 
   // Setup routes
   r := martini.NewRouter()
-  r.Get(`/albums`, func(r render.Render) {//, db *mgo.Database
-      al := structures.User{FirstName: "Write presentation", InstaUserID: 1234567890}
-      r.JSON(200, al)
-    })
-/*  r.Get(`/albums/:id`, GetAlbum)
-  r.Post(`/albums`, AddAlbum)
-  r.Put(`/albums/:id`, UpdateAlbum)
-  r.Delete(`/albums/:id`, DeleteAlbum)
-*/
+  r.Get(`/users/:insta_user_id`, func(r render.Render, params martini.Params) {//, db *mgo.Database
+    // rows, err := db.Query("SELECT * FROM user")
+    // checkErr(err)
+
+    // for rows.Next() {
+    //   var uid int
+    //   var username string
+    //   var department string
+    //   var created string
+    //   err = rows.Scan(&uid, &username, &department, &created)
+    //   checkErr(err)
+    //   fmt.Println(uid)
+    //   fmt.Println(username)
+    //   fmt.Println(department)
+    //   fmt.Println(created)
+    // }
+
+    fmt.Println(params['insta_user_id'])
+
+    r.JSON(200)
+  })
+  r.Get(`/testadd`, func(r render.Render, params martini.Params) {//, db *mgo.Database
+    s1 := structures.User{FirstName: "Write", SecondName: "presentation", InstaUserID: 1234567890}
+
+    stmt, err := db.Prepare("INSERT INTO user(first_name, second_name, insta_user_id) values(?,?,?)")
+    checkErr(err)
+    res, err := stmt.Exec(s1.FirstName, s1.SecondName, s1.InstaUserID)
+    checkErr(err)
+
+    r.JSON(200, s1)
+  })
 
   // Add the router action
   m.Action(r.Handle)
--- src/server_api/server_api.go
+++ src/server_api/server_api.go
@@ -1,9 +1,13 @@
 package server_api
 
 import (
+  "fmt"
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
   "structures"
+
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
 )
 
 
@@ -12,6 +16,12 @@ type Server *martini.ClassicMartini
 
 
 func RunServer() Server {//session *DatabaseSession
+
+  db, err := sql.Open("sqlite3", "dbfile.db")
+  checkErr(err)
+  defer db.Close()
+
+
   m := martini.Classic()
 
   // Setup middleware
@@ -26,10 +36,37 @@ func RunServer() Server {//session *DatabaseSession
 
   // Setup routes
   r := martini.NewRouter()
-  r.Get(`/albums`, func(r render.Render) {//, db *mgo.Database
-      al := structures.User{FirstName: "Write presentation", InstaUserID: 1234567890}
-      r.JSON(200, al)
-    })
+  r.Get(`/users/:insta_user_id`, func(r render.Render, params martini.Params) {//, db *mgo.Database
+    // rows, err := db.Query("SELECT * FROM user")
+    // checkErr(err)
+
+    // for rows.Next() {
+    //   var uid int
+    //   var username string
+    //   var department string
+    //   var created string
+    //   err = rows.Scan(&uid, &username, &department, &created)
+    //   checkErr(err)
+    //   fmt.Println(uid)
+    //   fmt.Println(username)
+    //   fmt.Println(department)
+    //   fmt.Println(created)
+    // }
+
+    fmt.Println(params['insta_user_id'])
+
+    r.JSON(200)
+  })
+  r.Get(`/testadd`, func(r render.Render, params martini.Params) {//, db *mgo.Database
+    s1 := structures.User{FirstName: "Write", SecondName: "presentation", InstaUserID: 1234567890}
+
+    stmt, err := db.Prepare("INSERT INTO user(first_name, second_name, insta_user_id) values(?,?,?)")
+    checkErr(err)
+    res, err := stmt.Exec(s1.FirstName, s1.SecondName, s1.InstaUserID)
+    checkErr(err)
+
+    r.JSON(200, s1)
+  })
 /*  r.Get(`/albums/:id`, GetAlbum)
   r.Post(`/albums`, AddAlbum)
   r.Put(`/albums/:id`, UpdateAlbum)
